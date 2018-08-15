9c4c8f7d296ecbb65026103727f2bf1a38bb4038
刘宗源 <940101379@qq.com>
2018-08-15 22:09:26 +0800

    feat(auto update): add simple auto update

--- src/main/index.js
+++ src/main/index.js
@@ -185,14 +185,12 @@ app.on('activate', () => {
  * https://simulatedgreg.gitbooks.io/electron-vue/content/en/using-electron-builder.html#auto-updating
  */
 
-/*
-import { autoUpdater } from 'electron-updater'
+import {autoUpdater} from 'electron-updater'
 
 autoUpdater.on('update-downloaded', () => {
-  autoUpdater.quitAndInstall()
+    autoUpdater.quitAndInstall()
 })
 
 app.on('ready', () => {
-  if (process.env.NODE_ENV === 'production') autoUpdater.checkForUpdates()
+    if (process.env.NODE_ENV === 'production') autoUpdater.checkForUpdates()
 })
- */
