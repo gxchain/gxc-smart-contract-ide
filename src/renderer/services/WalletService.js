57d619cf2bcbc8d1af14fafaff95c3ba97189557
刘宗源 <940101379@qq.com>
2018-08-15 19:51:33 +0800

    chore(service): change service i18n dependency

--- src/renderer/services/WalletService.js
+++ src/renderer/services/WalletService.js
@@ -5,6 +5,7 @@ import uniq from 'lodash/uniq'
 import some from 'lodash/some'
 import store from '@/store'
 import find from 'lodash/find'
+import i18n from '@/locales'
 // import Vue from 'vue'
 /**
  * get account information by name
@@ -22,84 +23,81 @@ const fetch_account = (account_name) => {
  */
 const import_account = (wifKey, password) => {
     return new Promise((resolve, reject) => {
-        import('@/locales').then(i18n => {
-            i18n = i18n.default
-            let password_aes = Aes.fromSeed(password)
-            let encryption_buffer = key.get_random_key().toBuffer()
-            let encryption_key = password_aes.encryptToHex(encryption_buffer)
-            let local_aes_private = Aes.fromSeed(encryption_buffer)
-            let encrypted_wifkey = local_aes_private.encryptToHex(wifKey)
-            let password_private = PrivateKey.fromSeed(password)
-            let password_pubkey = password_private.toPublicKey().toPublicKeyString() // used to validate password
+        let password_aes = Aes.fromSeed(password)
+        let encryption_buffer = key.get_random_key().toBuffer()
+        let encryption_key = password_aes.encryptToHex(encryption_buffer)
+        let local_aes_private = Aes.fromSeed(encryption_buffer)
+        let encrypted_wifkey = local_aes_private.encryptToHex(wifKey)
+        let password_private = PrivateKey.fromSeed(password)
+        let password_pubkey = password_private.toPublicKey().toPublicKeyString() // used to validate password
 
-            let imported = []
-            let exist = []
+        let imported = []
+        let exist = []
 
-            let private_key = PrivateKey.fromWif(wifKey)
-            let public_key = private_key.toPublicKey().toPublicKeyString()
-            resolve(Apis.instance().db_api().exec('get_key_references', [[public_key]]).then((resp) => {
-                if (resp.length > 0) {
-                    return uniq(resp[0])
+        let private_key = PrivateKey.fromWif(wifKey)
+        let public_key = private_key.toPublicKey().toPublicKeyString()
+        resolve(Apis.instance().db_api().exec('get_key_references', [[public_key]]).then((resp) => {
+            if (resp.length > 0) {
+                return uniq(resp[0])
+            } else {
+                throw new Error(i18n.t('importSetting.error.accountNotFound'))
+            }
+        }).then((account_ids) => {
+            return Apis.instance().db_api().exec('get_objects', [account_ids]).then((accounts) => {
+                if (accounts.length > 0) {
+                    let wallets = store.state.wallets
+                    accounts.forEach((account) => {
+                        let weight_threshold = account.active.weight_threshold
+                        // available key should have enough weight
+                        let isKeyAvailable = some(account.active.key_auths, function (key) {
+                            if (key[0] == public_key && key[1] >= weight_threshold) {
+                                return true
+                            }
+                            return false
+                        })
+                        if (isKeyAvailable) {
+                            // do not import a duplicate account
+                            let alreadyExist = some(wallets, function (wallet) {
+                                return wallet.account == account.name
+                            })
+                            if (!alreadyExist) {
+                                // 修改store状态
+                                let wallet = {
+                                    id: account.id,
+                                    account: account.name,
+                                    password_pubkey,
+                                    encryption_key,
+                                    encrypted_wifkey,
+                                    backup_date: null
+                                }
+                                imported.push(wallet)
+                                store.dispatch('appendWallet', wallet)
+                            } else {
+                                exist.push({
+                                    account: account.name
+                                })
+                            }
+                        }
+                    })
+
+                    // 成功
+                    if (imported.length > 0) {
+                        return {
+                            imported,
+                            exist
+                        }
+                    } else {
+                        if (exist.length > 0) {
+                            throw new Error(i18n.t('importSetting.error.exist'))
+                        } else {
+                            throw new Error(i18n.t('importSetting.error.accountNotFound'))
+                        }
+                    }
                 } else {
                     throw new Error(i18n.t('importSetting.error.accountNotFound'))
                 }
-            }).then((account_ids) => {
-                return Apis.instance().db_api().exec('get_objects', [account_ids]).then((accounts) => {
-                    if (accounts.length > 0) {
-                        let wallets = store.state.wallets
-                        accounts.forEach((account) => {
-                            let weight_threshold = account.active.weight_threshold
-                            // available key should have enough weight
-                            let isKeyAvailable = some(account.active.key_auths, function (key) {
-                                if (key[0] == public_key && key[1] >= weight_threshold) {
-                                    return true
-                                }
-                                return false
-                            })
-                            if (isKeyAvailable) {
-                                // do not import a duplicate account
-                                let alreadyExist = some(wallets, function (wallet) {
-                                    return wallet.account == account.name
-                                })
-                                if (!alreadyExist) {
-                                    // 修改store状态
-                                    let wallet = {
-                                        id: account.id,
-                                        account: account.name,
-                                        password_pubkey,
-                                        encryption_key,
-                                        encrypted_wifkey,
-                                        backup_date: null
-                                    }
-                                    imported.push(wallet)
-                                    store.dispatch('appendWallet', wallet)
-                                } else {
-                                    exist.push({
-                                        account: account.name
-                                    })
-                                }
-                            }
-                        })
-
-                        // 成功
-                        if (imported.length > 0) {
-                            return {
-                                imported,
-                                exist
-                            }
-                        } else {
-                            if (exist.length > 0) {
-                                throw new Error(i18n.t('importSetting.error.exist'))
-                            } else {
-                                throw new Error(i18n.t('importSetting.error.accountNotFound'))
-                            }
-                        }
-                    } else {
-                        throw new Error(i18n.t('importSetting.error.accountNotFound'))
-                    }
-                })
-            }))
-        })
+            })
+        }))
     })
 }
 
@@ -117,88 +115,82 @@ const deploy_contract = ({from = '', contractName = '', code = '', abi = '', fee
     let vm_version = '0'
 
     return new Promise((resolve, reject) => {
-        import('@/locales').then(i18n => {
-            i18n = i18n.default
-            resolve(Promise.all([fetch_account(from), unlock_wallet(from, password)]).then(results => {
-                let fromAcc = results[0]
-                if (!fromAcc) {
-                    throw new Error(i18n.t('contract.error.fromAccountNotExist'))
-                }
+        resolve(Promise.all([fetch_account(from), unlock_wallet(from, password)]).then(results => {
+            let fromAcc = results[0]
+            if (!fromAcc) {
+                throw new Error(i18n.t('contract.error.fromAccountNotExist'))
+            }
 
-                let tr = new TransactionBuilder()
+            let tr = new TransactionBuilder()
 
-                tr.add_operation(tr.get_type_operation('create_contract', {
-                    'fee': {
-                        'amount': 0,
-                        'asset_id': fee_id
-                    },
-                    'name': contractName,
-                    'account': fromAcc.id,
-                    vm_type,
-                    vm_version,
-                    code,
-                    abi
-                }))
-
-                return process_transaction(tr, from, password, broadcast).then((resp) => {
-                    // 如果不broadcast，返回为对象
-                    if (resp instanceof Array) {
-                        return fetch_account(contractName).then((account) => {
-                            resp[0].ext = {
-                                abi,
-                                from,
-                                contractName,
-                                contractId: account.id,
-                                fee: resp[0].trx.operations[0][1].fee
-                            }
-
-                            return resp
-                        })
-                    }
-                    // 时间、费用、费用类型
-                    return resp
-                })
+            tr.add_operation(tr.get_type_operation('create_contract', {
+                'fee': {
+                    'amount': 0,
+                    'asset_id': fee_id
+                },
+                'name': contractName,
+                'account': fromAcc.id,
+                vm_type,
+                vm_version,
+                code,
+                abi
             }))
-        })
+
+            return process_transaction(tr, from, password, broadcast).then((resp) => {
+                // 如果不broadcast，返回为对象
+                if (resp instanceof Array) {
+                    return fetch_account(contractName).then((account) => {
+                        resp[0].ext = {
+                            abi,
+                            from,
+                            contractName,
+                            contractId: account.id,
+                            fee: resp[0].trx.operations[0][1].fee
+                        }
+
+                        return resp
+                    })
+                }
+                // 时间、费用、费用类型
+                return resp
+            })
+        }))
     })
 }
 
 const call_contract = (from, target, act, fee_id, password, broadcast = true, amount = {}) => {
     return new Promise((resolve, reject) => {
-        import('@/locales').then(i18n => {
-            i18n = i18n.default
-            resolve(Promise.all([fetch_account(from), fetch_account(target), unlock_wallet(from, password)]).then(results => {
-                let fromAcc = results[0]
-                let contractAccount = results[1]
-                if (!fromAcc) {
-                    throw new Error(i18n.t('contract.error.fromAccountNotExist'))
-                }
+        resolve(Promise.all([fetch_account(from), fetch_account(target), unlock_wallet(from, password)]).then(results => {
+            let fromAcc = results[0]
+            let contractAccount = results[1]
+            if (!fromAcc) {
+                throw new Error(i18n.t('contract.error.fromAccountNotExist'))
+            }
 
-                if (!contractAccount) {
-                    throw new Error(i18n.t('contract.error.contractAccountNotExist'))
-                }
+            if (!contractAccount) {
+                throw new Error(i18n.t('contract.error.contractAccountNotExist'))
+            }
 
-                let tr = new TransactionBuilder()
-                let opts = {
-                    'fee': {
-                        'amount': 0,
-                        'asset_id': fee_id
-                    },
-                    'account': fromAcc.id,
-                    'contract_id': contractAccount.id,
-                    'method_name': act.method_name,
-                    'data': act.data
-                }
+            let tr = new TransactionBuilder()
+            let opts = {
+                'fee': {
+                    'amount': 0,
+                    'asset_id': fee_id
+                },
+                'account': fromAcc.id,
+                'contract_id': contractAccount.id,
+                'method_name': act.method_name,
+                'data': act.data
+            }
 
-                if (!!amount.amount) {
-                    let computedAmount
-                    computedAmount = gxcUtil.accMult(amount.amount, Math.pow(10, store.getters.assetMap[amount.asset_id].precision))
-                    opts.amount = {...amount, amount: computedAmount}
-                }
-                tr.add_operation(tr.get_type_operation('call_contract', opts))
-                return process_transaction(tr, from, password, broadcast)
-            }))
-        })
+            if (!!amount.amount) {
+                let computedAmount
+                computedAmount = gxcUtil.accMult(amount.amount, Math.pow(10, store.getters.assetMap[amount.asset_id].precision))
+                opts.amount = {...amount, amount: computedAmount}
+            }
+            tr.add_operation(tr.get_type_operation('call_contract', opts))
+            return process_transaction(tr, from, password, broadcast)
+        }))
     })
 }
 
@@ -228,31 +220,28 @@ const process_transaction = (tr, account, password, broadcast) => {
 
 const unlock_wallet = (account, password) => {
     return new Promise((resolve, reject) => {
-        import('@/locales').then(i18n => {
-            i18n = i18n.default
-            let wallets = store.state.wallets
-            let wallet = find(wallets, function (w) {
-                return w.account == account
-            })
-            wallet = Object.assign({}, wallet)
-            let password_private = PrivateKey.fromSeed(password)
-            let password_pubkey = password_private.toPublicKey().toPublicKeyString() // used to validate password
-            if (wallet == null) {
-                // TODO 本来想在最前面引入i18n，但是引入会导致循环依赖的问题
-                reject(new Error(i18n.t('unlock.messages.accountNotFound')))
-            } else if (password_pubkey != wallet.password_pubkey) {
-                reject(new Error(i18n.t('unlock.messages.invalidPassword')))
-            } else {
-                let password_aes = Aes.fromSeed(password)
-                let encryption_plainbuffer = password_aes.decryptHexToBuffer(wallet.encryption_key)
-                let aes_private = Aes.fromSeed(encryption_plainbuffer)
-                let wifKey = aes_private.decryptHexToText(wallet.encrypted_wifkey)
-                resolve({
-                    wifKey,
-                    wallet
-                })
-            }
+        let wallets = store.state.wallets
+        let wallet = find(wallets, function (w) {
+            return w.account == account
         })
+        wallet = Object.assign({}, wallet)
+        let password_private = PrivateKey.fromSeed(password)
+        let password_pubkey = password_private.toPublicKey().toPublicKeyString() // used to validate password
+        if (wallet == null) {
+            // TODO 本来想在最前面引入i18n，但是引入会导致循环依赖的问题
+            reject(new Error(i18n.t('unlock.messages.accountNotFound')))
+        } else if (password_pubkey != wallet.password_pubkey) {
+            reject(new Error(i18n.t('unlock.messages.invalidPassword')))
+        } else {
+            let password_aes = Aes.fromSeed(password)
+            let encryption_plainbuffer = password_aes.decryptHexToBuffer(wallet.encryption_key)
+            let aes_private = Aes.fromSeed(encryption_plainbuffer)
+            let wifKey = aes_private.decryptHexToText(wallet.encrypted_wifkey)
+            resolve({
+                wifKey,
+                wallet
+            })
+        }
     })
 }
 
