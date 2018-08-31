76a4ca0aa85f3ea7b48a317a2efb844924e2d8e5
刘宗源 <940101379@qq.com>
2018-08-31 11:07:30 +0800

    chore: fetch api json from github

--- src/renderer/store/modules/CppCompletion.js
+++ src/renderer/store/modules/CppCompletion.js
@@ -1,62 +1,5 @@
-const doc = {
-    types: [{
-        name: 'uint64_t'
-    }, {
-        name: 'int64_t'
-    }],
-    apis: [{
-        name: 'current_receiver',
-        returnType: 'uint64_t',
-        belong: '<graphenelib/action.h>',
-        description: {
-            'zh-CN': '返回当前合约账号的id',
-            'en-US': 'return current contract account ID'
-        }
-    }, {
-        name: 'get_action_asset_id',
-        returnType: 'uint64_t',
-        belong: '<graphenelib/action.h>',
-        description: {
-            'zh-CN': '返回本次调用向合约发送的资产id'
-        }
-    }, {
-        name: 'get_action_asset_amount',
-        returnType: 'uint64_t',
-        belong: '<graphenelib/action.h>',
-        description: '返回本次调用向合约发送的资产数量'
-    }, {
-        name: 'withdraw_asset',
-        returnType: 'void',
-        belong: '<graphenelib/asset.h>',
-        fields: [{
-            name: 'from',
-            type: 'uint64_t',
-            description: {
-                'zh-CN': '从哪个账号转账，一般是_self',
-                'en-US': 'transfer from which account, normally _self'
-            }
-        }, {
-            name: 'to',
-            type: 'uint64_t',
-            description: {
-                'zh-CN': '转账到哪个外部账户，必须只传账号的instance_id，比如外部账户是1.2.33，那么传33即可'
-            }
-        }, {
-            name: 'asset_id',
-            type: 'uint64_t',
-            description: {
-                'zh-CN': '指定转账的资产id，必须只传资产id的instance_id, 比如资产id是1.3.0， 那么传0即可'
-            }
-        }, {
-            name: 'amount',
-            type: 'int64_t',
-            description: {
-                'zh-CN': '转账金额，这个数字包含了资产的精度，比如想转1个GXC，那么应该写100000'
-            }
-        }],
-        description: '将当前合约的资产转移到外部账户'
-    }]
-}
+import axios from 'axios'
+import {b64_to_utf8} from 'gxc-frontend-base/src/script/util/index'
 
 const state = {
     lastSha: '',
@@ -64,6 +7,9 @@ const state = {
 }
 
 const mutations = {
+    UPDATE_SHA(state, sha) {
+        state.lastSha = sha
+    },
     UPDATE_COMPLETIONS(state, completions) {
         state.completions = completions
     }
@@ -92,9 +38,14 @@ function filterJson(json) {
 
 const actions = {
     updateCompletions({commit}) {
-        // fetch api json
-        // filter
-        commit('UPDATE_COMPLETIONS', filterJson(doc))
+        axios.get('https://api.github.com/repos/gxchain/Technical-Documents/contents/gxb_contract_api.json', {
+            headers: {'Accept': 'application/vnd.github.v3.object'}
+        }).then(res => {
+            if (res.data.sha !== state.lastSha) {
+                commit('UPDATE_SHA', res.data.sha)
+                commit('UPDATE_COMPLETIONS', filterJson(JSON.parse(b64_to_utf8(res.data.content))))
+            }
+        })
     }
 }
 
