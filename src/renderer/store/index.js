3636f0ca74aa04193a06ed2e55e40a037cfac5cc
刘宗源 <940101379@qq.com>
2018-09-04 20:55:36 +0800

    refactor(store): use electron store on electron env
    used to use localStorage in any case, which may not have enough space for files storage

--- src/renderer/store/index.js
+++ src/renderer/store/index.js
@@ -6,15 +6,39 @@ import getters from './getters.js'
 import actions from './actions.js'
 import modules from './modules'
 import createPersistedState from 'vuex-persistedstate/index'
+import ElectronStore from 'electron-store'
+import isElectron from 'is-electron'
 
 Vue.use(Vuex)
+
+const STORAGE_KEY = 'vuex'
+const electronStore = new ElectronStore()
+let presistPlugin
+
+if (isElectron()) {
+    presistPlugin = createPersistedState({
+        key: STORAGE_KEY,
+        storage: {
+            getItem: key => electronStore.get(key),
+            setItem: (key, value) => {
+                electronStore.set(key, value)
+            },
+            removeItem: key => electronStore.delete(key)
+        }
+    })
+} else {
+    presistPlugin = createPersistedState({
+        key: STORAGE_KEY
+    })
+}
+export const storage = isElectron() ? electronStore : localStorage
 
 const store = new Vuex.Store({
     state,
     mutations,
     getters,
     actions,
-    plugins: [createPersistedState()],
+    plugins: [presistPlugin],
     modules,
     strict: process.env.NODE_ENV !== 'production'
 })
