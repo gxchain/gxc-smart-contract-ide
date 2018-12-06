7b738aaa30bd1d029373795660f15a0c1a225f39
刘宗源 <940101379@qq.com>
2018-12-06 20:17:11 +0800

    fix: solve version error

--- src/renderer/storage/index.js
+++ src/renderer/storage/index.js
@@ -1,4 +1,4 @@
 import json from '../../../package.json'
 
 localStorage.getItem('lang') || localStorage.setItem('lang', 'en-US')
-localStorage.getItem('version') || localStorage.setItem('version', json.version)
+localStorage.setItem('version', json.version)
