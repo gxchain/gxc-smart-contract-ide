b04e4e97284e2ac56e39e511c1423348b8e991bf
刘宗源 <940101379@qq.com>
2019-01-09 15:38:21 +0800

    fix: method name 12 character deploy error

--- src/renderer/util/serializer.js
+++ src/renderer/util/serializer.js
@@ -1,43 +1,4 @@
-import {types, ops} from 'gxbjs/es/index'
-import ByteBuffer from 'bytebuffer'
-import {cloneDeep} from 'lodash'
-
-function isArrayType(type) {
-    return type.indexOf('[]') !== -1
-}
-
-const serializeCallData = (action, params, abi) => {
-    abi = cloneDeep(abi)
-    let struct = abi.structs.find(s => s.name === action)
-    let b = new ByteBuffer(ByteBuffer.DEFAULT_CAPACITY, ByteBuffer.LITTLE_ENDIAN)
-    struct.fields.forEach(f => {
-        let value = params[f.name]
-        let isArrayFlag = false
-        if (isArrayType(f.type)) {
-            isArrayFlag = true
-            f.type = f.type.split('[')[0]
-        }
-
-        let type = types[f.type]
-        if (!type) {
-            let t = abi.types.find(t => t.new_type_name === f.type)
-            if (t) {
-                type = types[t.type]
-            }
-        }
-        if (!type) {
-            type = ops[f.type]
-        }
-
-        if (type) {
-            if (isArrayFlag) {
-                type = types.set(type)
-            }
-            type.appendByteBuffer(b, value)
-        }
-    })
-    return Buffer.from(b.copy(0, b.offset).toBinary(), 'binary')
-}
+import {serializeCallData} from 'gxc-frontend-base/src/script/util/serializer'
 
 export default {
     serializeCallData
